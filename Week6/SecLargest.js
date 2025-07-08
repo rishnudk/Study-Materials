eedf9d5cec895f579803e85497369729d1496bb9
Rishnu DK <rishnukrishnan@gmail.com>
2025-07-08 12:58:45 +0530

    new practices added

--- Week6/SecLargest.js
+++ Week6/SecLargest.js
@@ -1,7 +1,8 @@
-let str = [42,765,234,89,32]
+let str = [42,765,234,765,89,32]
 
 let largest = -Infinity
 let sec = -Infinity
+let third = 0
 
 for (let i =0;i<str.length;i++){
     if (str[i] > largest) {
@@ -10,8 +11,11 @@ for (let i =0;i<str.length;i++){
     }
     else if (str[i]>sec && str[i] !== largest) {
         sec = str[ i]
-    }
+    } else if ( str[i] > third && str[i] !== ( largest || sec) ) {
+        third = str[i]
+    } 
     
 }
 console.log(largest)
 console.log(sec)
+console.log(third)
